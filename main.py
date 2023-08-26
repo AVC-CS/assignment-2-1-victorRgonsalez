a861e518804f85b0cbf749c82a3edcdd59f6dc4d
victorRgonsalez <142199178+victorRgonsalez@users.noreply.github.com>
2023-08-26 06:53:35 +0000

    First Commit

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ def main():
     ##################################################
     """
 
+    m_perc = int(input("please enter the amount of males in your class \t"))
+    f_perc = int(input("please enter the amount of females in your class \t"))
+
+    print(f'Total number of students: \t {m_perc + f_perc}')
+    print(f'The number of males and females: \t {m_perc} {f_perc}')
+
+    sum_m = (m_perc * 100) / (m_perc + f_perc)
+    sum_f = (f_perc * 100) / (m_perc + f_perc)
+    print(f'The percentage of males and females: \t {sum_m:.2f} {sum_f:.2f}')
+    m_perc = sum_m
+    f_perc = sum_f
+
     """
     ########################################
     # Do not delete the return statement
